4c2b30ef848b2f46c5d820565b02a9b4c68c134a
sanjaykazi <sanjaykazi1149@gmail.com>
2024-11-15 17:17:59 +0530

    useEffect done

--- useEffect/src/App.jsx
+++ useEffect/src/App.jsx
@@ -1,34 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  //variation 1
+  //runs on every render before rendering
+  const [count, setCount] =useState(0);
+  const [total, setTotal] =useState(1);
+  // useEffect(() => {
+  //   alert("alert on every render")
+  // })
+  function handleCount(){
+    setCount(count+1);
+  }
+  function handleTotal(){
+    setTotal(total+1);
+  }
+
+  //variation 2
+  // that runs only on first render
+  // useEffect(() => {
+  //   alert("I will only run on first render")
+  // }, [])
+
+  //variation 3
+  // runs only on change of dependency list variables
+  // useEffect(() => {
+  //   alert("i will run when count is changed")
+  // }, [count])
+
+  //variation 4
+  // depends on multiple variables
+  // useEffect(() => {
+  //   alert("I will run when count/ total is changed")
+  // }, [count, total])
+  
+  //variation 5
+  // using clean-up function
+  useEffect(() => {    
+    alert('count is updated')
+    return () => {
+      alert("count is unmounted from UI")
+    }
+  }, [count])
+  
+  
+  
+  
+
+  // first: side-effect function
+  // second: clean-up function
+  // third: comma separated dependencies list
+  // useEffect( () => {
+  //   first
+
+  //   return () => {
+  //     second
+  //   }
+  // }, [third])
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <button onClick={handleCount}>
+        Click Me
+      </button>
+      <br/>
+      Count: {count}
+      <br/>
+      <button onClick={handleTotal}>
+        Click Me
+      </button>
+      <br/>
+      Total: {total}
+    </div>
   )
 }
 
