c97aa0d0928dc9f9a9c8a4177bf2287da866bb35
sanjaykazi <sanjaykazi1149@gmail.com>
2024-11-16 00:19:38 +0530

    modified readme.md

---
+++ useContext/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+// 1. create context
+// 2. privde context
+// 3. use context
+
+// it helps avoiding prop drilling.
+
+function App() {
+
+  return (
+    <div>
+      Hello Jee
+    </div>
+  )
+}
+
+export default App
